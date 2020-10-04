503780dcff27b8dc57568866a05ffa676f4a8cab
jason skipper <52185268+jasonskipper@users.noreply.github.com>
2020-10-03 23:11:42 -0400

    Create Carrots.java

---
+++ Carrots.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Carrots {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                int h = scanner.nextInt();
+                int m = scanner.nextInt();          
+                System.out.println(m);
+            }
+}
