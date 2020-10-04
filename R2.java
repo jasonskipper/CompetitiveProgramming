df06e208e24638ab71beb2987de5cc67ef4cbbf8
jason skipper <52185268+jasonskipper@users.noreply.github.com>
2020-10-03 23:11:23 -0400

    Create R2.java

---
+++ R2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class R2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println((2*num2)-num1);
+    }
+}
