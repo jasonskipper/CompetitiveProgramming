b62c3972e7a19ad3ae9f3d61293e551f34249ab7
jason skipper <52185268+jasonskipper@users.noreply.github.com>
2021-01-31 22:23:12 -0500

    Create ValidateSubsequence.py

---
+++ AlgoExpert/python/ValidateSubsequence.py
@@ -0,0 +1,8 @@
+def isValidSubsequence(array, sequence):
+	array_index = 0
+	sequence_index = 0
+	while array_index < len(array) and sequence_index < len(sequence):
+		if array[array_index] == sequence[sequence_index]:
+			sequence_index += 1
+		array_index += 1
+	return sequence_index == len(sequence)
